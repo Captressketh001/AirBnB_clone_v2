26586b53fa830d8cd4fc9c71d334bf6b3d08a6ca
Captressketh001exitgit config --global user.name Captressketh001 <shininglite2012@gmail.com>
2023-04-21 09:52:51 -0700

    nginx configuring file

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+
+# install nginx if not previosul installed
+if [ ! -x /usr/sbin/nginx ]; then
+	sudo apt-get update -y -qq &\
+		sudo apt-get install -y nginx
+fi
+
+# create directories using -p
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
+
+# Create a fake HTML file
+echo "<h2 style='text-align:center'>Welcome to captressketh.tech</h2>" | sudo dd status=none of=/data/web_static/releases/test/index.html
+
+# create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration
+sudo sed i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart nginx
+sudo service nginx restart
