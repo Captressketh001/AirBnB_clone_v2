9cad0aee439c74908160d355dee022eed9dc167c
Captressketh001exitgit config --global user.name Captressketh001 <shininglite2012@gmail.com>
2023-04-21 09:57:31 -0700

    added -to i

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,7 +20,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration
-sudo sed i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 # Restart nginx
 sudo service nginx restart
