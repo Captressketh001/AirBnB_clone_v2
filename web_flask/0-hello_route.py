2451c45fd064772c684891be737981aad0cac4cf
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-05-04 10:24:29 +0100

    started flask application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
-from web_flask import app
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
     """Return HBNB when routed to /"""
     return "Hello HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
