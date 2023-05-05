8ba061244e570d14e326d1bb7ee9847734634657
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-05-05 02:11:19 +0100

    cities by state

---
+++ web_flask/9-states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def all_states():
+    states = storage.all('State').values()
+    """Return list of states"""
+    return render_template('10-hbnb_filters.html', states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def filter_states(id):
+    states = storage.all('State').values()
+    state = states.query.get_or_404(id)
+    return render_template('10-hbnb_filters.html', filter_states=state)
+
+@app.teardown_appcontext
+def teardown(self):
+    """Remove alchemy session"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
